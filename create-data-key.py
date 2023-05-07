e774e8c7c8ea8fe265f9f630d41b61920d6903dd
Scott Eissfeldt <scott@eissfeldt.com>
2023-05-07 09:35:16 -0700

    create data key and return plain text and encrypted

---
+++ create-data-key.py
@@ -0,0 +1,24 @@
+def create_data_key(cmk_id, key_spec='AES_256'):
+    """Generate a data key to use when encrypting and decrypting data
+
+    :param cmk_id: KMS CMK ID or ARN under which to generate and encrypt the
+    data key.
+    :param key_spec: Length of the data encryption key. Supported values:
+        'AES_128': Generate a 128-bit symmetric key
+        'AES_256': Generate a 256-bit symmetric key
+    :return Tuple(EncryptedDataKey, PlaintextDataKey) where:
+        EncryptedDataKey: Encrypted CiphertextBlob data key as binary string
+        PlaintextDataKey: Plaintext base64-encoded data key as binary string
+    :return Tuple(None, None) if error
+    """
+
+    # Create data key
+    kms_client = boto3.client('kms')
+    try:
+        response = kms_client.generate_data_key(KeyId=cmk_id, KeySpec=key_spec)
+    except ClientError as e:
+        logging.error(e)
+        return None, None
+
+    # Return the encrypted and plaintext data key
+    return response['CiphertextBlob'], base64.b64encode(response['Plaintext'])
